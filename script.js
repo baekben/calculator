7f7242fff4a05a02ce1ebe8e20c8a8465dfe13f6
Ben <ben10baek@hotmail.com>
2020-01-30 21:19:02 -0800

    Two number problems now solvable

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const calculator = document.querySelector('.calculator');
 const buttons = document.querySelectorAll('.buttons');
+var display = document.querySelector('.display');
+var nums = new Array();
+var answer;
+var displayError = false;
 buttons.forEach(button => button.addEventListener('click', input));
-
 function input(e) {
   const button = e.target;
   const type = button.className;
-  if (type == 'number') {
-    console.log(button.getAttribute('value'));
-  } else if (type == 'operator') {
-    console.log(button.getAttribute('value'));
-  } else {
-    console.log(button.getAttribute('value'));
+  const value = button.getAttribute('value');
+  console.log(value);
+  // if ((displayError = true)) {
+  //   display.textContent = '';
+  //   displayError = false;
+  // }
+  display.textContent = value;
+  switch (type) {
+    case 'number':
+      nums.push(Number(value));
+
+      console.log(nums);
+      break;
+    case 'operator':
+      nums.push(value);
+      break;
+    case 'equal':
+      console.log(nums[0]);
+      console.log(nums[1]);
+      operate(nums[1], nums[0], nums[2]);
+      nums = [];
+      break;
+    case 'clear':
+      display.textContent = '';
+      break;
   }
 }
 
@@ -30,8 +52,6 @@ function divide(a, b) {
 function operate(operator, num1, num2) {
   //solves the problem
   var total;
-  num1 = Number(num1);
-  num2 = Number(num2);
   switch (operator) {
     case '+':
       total = add(num1, num2);
@@ -46,5 +66,6 @@ function operate(operator, num1, num2) {
       total = divide(num1, num2);
       break;
   }
+  display.textContent = total.toString();
   console.log(total);
 }
